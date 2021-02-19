b3fd232f4eabaa99103f5dc36c07ac505ff41ec0
KoalaBrasileiro <55700022+KoalaBrasileiro@users.noreply.github.com>
2021-02-18 21:14:59 -0300

    Mymaps

---
+++ app/src/main/java/com/jm/mymaps/adapter/UserAdapter.kt
@@ -0,0 +1,25 @@
+package com.jm.mymaps.adapter
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
+import com.jm.mymaps.R
+
+class UserAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
+
+
